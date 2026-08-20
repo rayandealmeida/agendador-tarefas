e12ab27d397e1cdca96e3a82cfc18f8635eb631d
Rayan <rayan.almeida0@outlook.com>
2026-08-20 18:00:29 -0300

    Adicionadas configurações de segurança

---
+++ src/main/java/com/rayan/agendadortarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.rayan.agendadortarefas.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+    private String email;
+    private String senha;
+}
