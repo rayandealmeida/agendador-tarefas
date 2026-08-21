520e1a8699db560e4d1648c00ccb0140d7fc692c
Rayan <rayan.almeida0@outlook.com>
2026-08-20 23:19:16 -0300

    Adicionados métodos de delete, patch de status e put de tarefas

--- src/main/java/com/rayan/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/rayan/agendadortarefas/controller/TarefasController.java
@@ -2,7 +2,9 @@ package com.rayan.agendadortarefas.controller;
 
 import com.rayan.agendadortarefas.business.TarefasService;
 import com.rayan.agendadortarefas.business.dto.TarefasDTO;
+import com.rayan.agendadortarefas.infrastructure.enums.StatusNotificaoEnum;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +20,37 @@ public class TarefasController {
 
     @PostMapping
     public ResponseEntity<TarefasDTO> gravarTarefas(@RequestBody TarefasDTO dto,
-                                                    @RequestHeader("Authorization")String token){
+                                                    @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefasService.gravarTarefa(token, dto));
     }
 
     @GetMapping("/eventos")
     public ResponseEntity<List<TarefasDTO>> buscaListaDeTarefaPorPeriodo(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataInicial,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataFinal){
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal) {
         return ResponseEntity.ok(tarefasService.buscarTarefasAgendadasPorPeriodo(dataInicial, dataFinal));
     }
 
     @GetMapping
-    public ResponseEntity<List<TarefasDTO>> buscarTarefasPorEmail(@RequestHeader("Authorization")String token){
+    public ResponseEntity<List<TarefasDTO>> buscarTarefasPorEmail(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefasService.buscarTarefasPorEmail(token));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTarefaPorId(@RequestParam("id") String id) {
+        tarefasService.deletaTarefaPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefasDTO> alterarStatusNotificacao(@RequestParam("status") StatusNotificaoEnum statusNotificaoEnum,
+                                                               @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefasService.alteraStatus(statusNotificaoEnum, id));
+    }
+
+    @PutMapping
+    public ResponseEntity<TarefasDTO> updateTarefas(@RequestBody TarefasDTO dto,
+                                                    @RequestParam("id")String id){
+        return ResponseEntity.ok(tarefasService.updateTarefas(dto, id));
     }
 }
